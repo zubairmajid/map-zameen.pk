38331171035aae3fa7a377cec91956bd6e89dab1
zubairmajid <zubairmajid1111@gmail.com>
2024-10-30 15:07:53 +0500

    Add files via upload

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -2,7 +2,7 @@
 export function checkAuth() {
     // Redirect to login page if session token is missing
     if (!sessionStorage.getItem("sessionToken")) {
-        window.location.href = "https://mustafapro.com/login.html";
+        window.location.href = "https://test.mustafapro.com/login.html";
     }
 
     // Clear session token on reload or new tab
