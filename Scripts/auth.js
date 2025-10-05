c6151eae3811d1f3ee565e857be0407be89c5a64
zubairmajid <zubairmajid1111@gmail.com>
2025-10-05 13:51:49 +0500

    Update auth.js

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -1,12 +1,10 @@
 // auth.js
 export function checkAuth() {
-    // Redirect to login page if session token is missing
-    if (!sessionStorage.getItem("sessionToken")) {
-        window.location.href = "https://test.mustafapro.com/login.html";
-    }
+   
 
     // Clear session token on reload or new tab
     window.addEventListener("beforeunload", () => {
         sessionStorage.removeItem("sessionToken");
     });
 }
+
